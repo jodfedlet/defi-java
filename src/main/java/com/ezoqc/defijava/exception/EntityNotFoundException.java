951e443fd52c76a348a36e3998e46762a5ab81ff
Jod Fedlet PIERRE <fedletpierre15@gmail.com>
2023-05-05 13:19:56 +0000

    refactor: update exception message

--- src/main/java/com/ezoqc/defijava/exception/EntityNotFoundException.java
+++ src/main/java/com/ezoqc/defijava/exception/EntityNotFoundException.java
@@ -3,7 +3,7 @@ package com.ezoqc.defijava.exception;
 public class EntityNotFoundException extends RuntimeException {
 
     public EntityNotFoundException() {
-        super("L entité demandée n a pas été trouvée");
+        super("L'entité demandée n'a pas été trouvée");
     }
 
     public EntityNotFoundException(String message) {
--- src/main/java/com/ezoqc/defijava/exception/InvalidArgumentException.java
+++ src/main/java/com/ezoqc/defijava/exception/InvalidArgumentException.java
@@ -3,7 +3,7 @@ package com.ezoqc.defijava.exception;
 public class InvalidArgumentException extends Exception {
 
     public InvalidArgumentException() {
-        super("L arqument reçu est incorrect.");
+        super("L'argument reçu est incorrect.");
     }
 
     public InvalidArgumentException(String message) {
