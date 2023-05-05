11e3fa5234bb31583ccf7cf13ac12889d729dd8e
jodfedlet <fedletpierre15@gmail.com>
2023-05-05 12:04:43 -0300

    fix: use correct status code in exceptions

--- src/main/java/com/ezoqc/defijava/exception/InvalidArgumentException.java
+++ src/main/java/com/ezoqc/defijava/exception/InvalidArgumentException.java
@@ -1,5 +1,9 @@
 package com.ezoqc.defijava.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidArgumentException extends Exception {
 
     public InvalidArgumentException() {
